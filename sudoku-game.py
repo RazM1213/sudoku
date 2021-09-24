716562d4c512622da0edad2c03b56af9f7dc7bd3
Raz Matzliah <razm1213@gmail.com>
2021-09-24 21:14:28 +0300

    added grid to the game layout

--- sudoku-game.py
+++ sudoku-game.py
@@ -8,7 +8,17 @@ def main():
     pygame.init()
     win = pygame.display.set_mode((WIDTH,WIDTH))
     pygame.display.set_caption('Sudoku')
+    win.fill(background_color)
+    for i in range(0,10):
+        if (i % 3 == 0):
+            #Every third line is bold
+            pygame.draw.line(win, (0, 0, 0), (50 + 50 * i, 50), (50 + 50 * i, 500), 4)
+            pygame.draw.line(win, (0, 0, 0), (50, 50 + 50 * i), (500, 50 + 50 * i), 4)
 
+        pygame.draw.line(win, (0, 0, 0), (50 + 50 * i, 50), (50 + 50 * i,500), 2)
+        pygame.draw.line(win, (0, 0, 0), (50, 50 + 50 * i), (500, 50 + 50 * i), 2)
+
+    pygame.display.update()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
